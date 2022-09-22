1d56584cbcc839c2caae6c723d04175311a17813
Gál Gergely <gt8yb1@INF.ELTE.HU>
2022-09-22 11:28:52 +0200

    2.gyak

--- Python/Gyak_2/gyak_2.py
+++ Python/Gyak_2/gyak_2.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import pyautogui
+import time
+ 
+def cryBabyCry():
+    for line in open("input.txt", "r"):
+        time.sleep(0.5)
+        for x in range(1,100):
+            pyautogui.typewrite(line)
+            pyautogui.press("enter")
+         
+        
+def oneInput(x):
+    return 3*3
+        
+#time.sleep(5) 
+#cryBabyCry();
